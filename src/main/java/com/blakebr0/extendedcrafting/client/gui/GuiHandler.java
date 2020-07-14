a2d65f00963553e5d909a102e75fb1a4acdfe735
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-07-14 17:32:38 +0100

    Fix GuiHandler nullability warnings.

--- src/main/java/com/blakebr0/extendedcrafting/client/gui/GuiHandler.java
+++ src/main/java/com/blakebr0/extendedcrafting/client/gui/GuiHandler.java
@@ -5,6 +5,7 @@ import com.blakebr0.extendedcrafting.client.container.automationinterface.Contai
 import com.blakebr0.extendedcrafting.client.gui.automationinterface.GuiAutomationInterface;
 import com.blakebr0.extendedcrafting.tile.*;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
@@ -28,54 +29,58 @@ public class GuiHandler implements IGuiHandler {
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		TileEntity tile = world.getTileEntity(new BlockPos(x, y, z));
+		if (tile == null && ID != HANDHELD_TABLE) {
+			return null;
+		}
 		switch (ID) {
 			case CRAFTING_CORE:
-				return new GuiCraftingCore((TileCraftingCore) world.getTileEntity(new BlockPos(x, y, z)), new ContainerCraftingCore(player.inventory, (TileCraftingCore) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiCraftingCore((TileCraftingCore) tile, new ContainerCraftingCore(player.inventory, (TileCraftingCore) tile));
 			case HANDHELD_TABLE:
 				return new GuiHandheldTable(new ContainerHandheldTable(player.inventory, world));
 			case AUTOMATION_INTERFACE:
-				TileAutomationInterface aI = (TileAutomationInterface) world.getTileEntity(new BlockPos(x, y, z));
-				if (aI != null)
-					return new GuiAutomationInterface(new ContainerAutomationInterface(player.inventory, aI));
+				return new GuiAutomationInterface(new ContainerAutomationInterface(player.inventory, (TileAutomationInterface) tile));
 			case BASIC_TABLE:
-				return new GuiBasicTable(new ContainerBasicTable(player.inventory, (TileBasicCraftingTable) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiBasicTable(new ContainerBasicTable(player.inventory, (TileBasicCraftingTable) tile));
 			case ADVANCED_TABLE:
-				return new GuiAdvancedTable(new ContainerAdvancedTable(player.inventory, (TileAdvancedCraftingTable) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiAdvancedTable(new ContainerAdvancedTable(player.inventory, (TileAdvancedCraftingTable) tile));
 			case ELITE_TABLE:
-				return new GuiEliteTable(new ContainerEliteTable(player.inventory, (TileEliteCraftingTable) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiEliteTable(new ContainerEliteTable(player.inventory, (TileEliteCraftingTable) tile));
 			case ULTIMATE_TABLE:
-				return new GuiUltimateTable(new ContainerUltimateTable(player.inventory, (TileUltimateCraftingTable) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiUltimateTable(new ContainerUltimateTable(player.inventory, (TileUltimateCraftingTable) tile));
 			case COMPRESSOR:
-				return new GuiCompressor((TileCompressor) world.getTileEntity(new BlockPos(x, y, z)), new ContainerCompressor(player.inventory, (TileCompressor) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiCompressor((TileCompressor) tile, new ContainerCompressor(player.inventory, (TileCompressor) tile));
 			case ENDER_CRAFTER:
-				return new GuiEnderCrafter((TileEnderCrafter) world.getTileEntity(new BlockPos(x, y, z)), new ContainerEnderCrafter(player.inventory, (TileEnderCrafter) world.getTileEntity(new BlockPos(x, y, z))));
+				return new GuiEnderCrafter((TileEnderCrafter) tile, new ContainerEnderCrafter(player.inventory, (TileEnderCrafter) tile));
 		}
 		return null;
 	}
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		TileEntity tile = world.getTileEntity(new BlockPos(x, y, z));
+		if(tile == null && ID != HANDHELD_TABLE) {
+			return null;
+		}
 		switch (ID) {
 			case CRAFTING_CORE:
-				return new ContainerCraftingCore(player.inventory, (TileCraftingCore) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerCraftingCore(player.inventory, (TileCraftingCore) tile);
 			case HANDHELD_TABLE:
 				return new ContainerHandheldTable(player.inventory, world);
 			case AUTOMATION_INTERFACE:
-				TileAutomationInterface aI = (TileAutomationInterface) world.getTileEntity(new BlockPos(x, y, z));
-				if (aI != null)
-					return new ContainerAutomationInterface(player.inventory, aI);
+				return new ContainerAutomationInterface(player.inventory, (TileAutomationInterface) tile);
 			case BASIC_TABLE:
-				return new ContainerBasicTable(player.inventory, (TileBasicCraftingTable) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerBasicTable(player.inventory, (TileBasicCraftingTable) tile);
 			case ADVANCED_TABLE:
-				return new ContainerAdvancedTable(player.inventory, (TileAdvancedCraftingTable) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerAdvancedTable(player.inventory, (TileAdvancedCraftingTable) tile);
 			case ELITE_TABLE:
-				return new ContainerEliteTable(player.inventory, (TileEliteCraftingTable) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerEliteTable(player.inventory, (TileEliteCraftingTable) tile);
 			case ULTIMATE_TABLE:
-				return new ContainerUltimateTable(player.inventory, (TileUltimateCraftingTable) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerUltimateTable(player.inventory, (TileUltimateCraftingTable) tile);
 			case COMPRESSOR:
-				return new ContainerCompressor(player.inventory, (TileCompressor) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerCompressor(player.inventory, (TileCompressor) tile);
 			case ENDER_CRAFTER:
-				return new ContainerEnderCrafter(player.inventory, (TileEnderCrafter) world.getTileEntity(new BlockPos(x, y, z)));
+				return new ContainerEnderCrafter(player.inventory, (TileEnderCrafter) tile);
 		}
 		return null;
 	}
