d19a51979ec0ab51fb32a6c3dbe779f56e7bd248
eutropius225 <11538216+eutropius225@users.noreply.github.com>
2020-09-02 11:15:49 +0100

    Use CraftTweakerMC#getIngredient to get an Ingredient from the IIngredient. Fixes #3

--- src/main/java/com/blakebr0/extendedcrafting/compat/crafttweaker/CraftTweakerUtils.java
+++ src/main/java/com/blakebr0/extendedcrafting/compat/crafttweaker/CraftTweakerUtils.java
@@ -2,9 +2,7 @@ package com.blakebr0.extendedcrafting.compat.crafttweaker;
 
 import crafttweaker.api.data.IData;
 import crafttweaker.api.item.IIngredient;
-import crafttweaker.api.item.IItemStack;
 import crafttweaker.api.minecraft.CraftTweakerMC;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.util.NonNullList;
@@ -36,26 +34,7 @@ public class CraftTweakerUtils {
 
     @Nonnull
     public static Ingredient toIngredient(@Nullable IIngredient ingredient) {
-        return ingredient == null ? Ingredient.EMPTY : new CTIngredientWrapper(ingredient);
+        return CraftTweakerMC.getIngredient(ingredient);
     }
 
-    private static class CTIngredientWrapper extends Ingredient {
-
-        private final IIngredient ingredient;
-
-        private CTIngredientWrapper(IIngredient ingredient) {
-            super(CraftTweakerMC.getItemStacks(ingredient.getItemArray()));
-            this.ingredient = ingredient;
-        }
-
-        @Override
-        public boolean apply(@Nullable ItemStack stack) {
-            if(stack == null) {
-                return ingredient.matches((IItemStack) null);
-            }
-            stack = stack.copy();
-            stack.setCount(ingredient.getAmount());
-            return ingredient.matches(CraftTweakerMC.getIItemStack(stack));
-        }
-    }
 }
